93cc947a67829122669910ea99865d81a41bb25e
Андрей Хухрянский <andrejhuhranskij@Andrejs-Mac-mini.local>
2021-11-03 14:32:17 +0200

    adds new tests

--- TRAINEE/UI_test_task_1/spec/main_menu_spec.rb
+++ TRAINEE/UI_test_task_1/spec/main_menu_spec.rb
@@ -15,24 +15,24 @@ RSpec.describe 'Active page elements' do
 
   # Positive testing
 
-  it 'verify that can not find unexisted info' do
+  it 'verifies that user can not unexisting info' do
     main_pg.enter_data(browser, INVALID_DATA)
     main_pg.submit_search(browser).click
     expect(main_pg.posts(browser).text) == 'Nothing Found'
   end
 
-  it 'verify that can find existed info' do
+  it 'verifies that user can find existing info' do
     main_pg.search_field(browser).clear
     main_pg.enter_data(browser, VALID_DATA)
     main_pg.submit_search(browser).click
     expect(main_pg.posts(browser).text) == 'Search Results for: ' + VALID_DATA
   end
 
-  it 'verify that user can read existed data' do
+  it 'verify that user can read existing data' do
     main_pg.posts(browser).click
     expect(main_pg.url_include?(browser, VALID_DATA))
   end
 
-  
+
 
 end
